e84b5c24c706453030152753160ac12d71807fe4
adamrazey <adam.razey@doane.edu>
2026-06-28 09:49:56 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RefactorClassRosterApp
+{
+
+	class Person
+	{
+		private string firstName;
+		private string lastName;
+
+		public Person()
+		{
+			firstName = "";
+			lastName = "";
+		}
+
+		public Person(string newfirstName, string newlastName)
+		{
+			firstName = newfirstName;
+			lastName = newlastName;
+		}
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+	}
+}
