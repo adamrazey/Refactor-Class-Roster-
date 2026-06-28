fe424047ca4b2c21408d739e14cf19a85f154d34
adamrazey <adam.razey@doane.edu>
2026-06-28 09:51:36 -0500

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RefactorClassRosterApp
+{
+	class Student : Person
+	{
+		private string classRank;
+		
+		public Student() : base()
+		{
+			classRank = "";
+		}
+
+		public Student(string newFirstName, string newLastName, string newClassRank) : base(newFirstName, newLastName)
+		{
+			classRank = newClassRank;
+		}
+
+		public string ClassRank
+		{
+			get{ return classRank; }
+			set{ classRank = value;  }
+		}
+
+		public override string ToString()
+		{
+			return FirstName + " " + LastName + " - " + ClassRank;
+		}
+	}
+}
+
