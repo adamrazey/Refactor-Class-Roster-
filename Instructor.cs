b13fd715451b8c16b353537f75c474079481014e
adamrazey <adam.razey@doane.edu>
2026-06-28 09:49:04 -0500

    Add files via upload

---
+++ Instructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefactorClassRosterApp
+{
+	class Instructor : Person
+	{
+
+		private string contactInfo;
+
+		public Instructor() : base()
+		{
+			contactInfo = "";
+		}
+
+		public Instructor(string newFirstName, string newLastName, string newContactInfo) : base(newFirstName, newLastName)
+		{
+			contactInfo = newContactInfo;
+		}
+
+		public string ContactInfo
+		{
+			get { return contactInfo; }
+			set { contactInfo = value; }
+		}
+
+		public override string ToString()
+		{
+			return FirstName + " " + LastName + " - " + ContactInfo;
+		}
+
+
+	}
+}
